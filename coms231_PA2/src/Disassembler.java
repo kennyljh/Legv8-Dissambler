02562582feda04bca1652f1f657c6cba6012000d
Kenny Leong <kennyljh@iastate.edu>
2024-12-01 19:26:02 -0600

    progress

--- coms231_PA2/src/Disassembler.java
+++ coms231_PA2/src/Disassembler.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -11,7 +13,7 @@ import java.util.Map;
 
 public class Disassembler {
 
-    private Map<Integer, String> bytesToInstructions = new HashMap<>();
+    private Map<Integer, String> bytesToInstructions = new LinkedHashMap<>();
     byte[] bytes = null;
 
 
@@ -51,19 +53,34 @@ public class Disassembler {
             return;
         }
 
+        for (int i = 0; i < bytes.length; i += 4){
+
+            int instructionBitOf32 = ((bytes[i] << 24) |
+                                        (bytes[i + 1] << 16) |
+                                        (bytes[i + 2 << 8]) |
+                                        (bytes[i + 3]));
+
+            String instructionType = getInstructionType((instructionBitOf32 >> 21) & 0x7ff);
+
+            switch (instructionType){
+
+
+            }
+        }
+
 
     }
 
     /**
-     * Determines instruciton type from opcode binary literal
+     * Determines instruction type from opcode binary literal
      * @param opcodeOfEleven 11 bit opcode
      * @return instruction type
      */
     private String getInstructionType(int opcodeOfEleven){
 
-        int opcodeOfTen = opcodeOfEleven >> 1;
-        int opcodeOfEight = opcodeOfEleven >> 3;
-        int opcodeOfSix = opcodeOfEleven >> 5;
+        int opcodeOfTen = (opcodeOfEleven >> 1) & 0x3ff;
+        int opcodeOfEight = (opcodeOfEleven >> 3) & 0xff;
+        int opcodeOfSix = (opcodeOfEleven >> 5) & 0x3f;
 
         if (opcodeOfEleven >= 0b00011110001 && opcodeOfEleven <= 0b11111100010){
 
@@ -89,6 +106,118 @@ public class Disassembler {
 
         return null;
     }
+
+    /**
+     * Decodes R-type instructions:
+     *     ADD
+     *     AND
+     *     BR
+     *     EOR
+     *     LSL
+     *     LSR
+     *     ORR
+     *     SUB
+     *     SUBS
+     *     MUL
+     *     // extras
+     *     PRNT
+     *     PRNL
+     *     DUMP
+     *     HALT
+     *
+     * @param instructionOf32
+     * @return
+     */
+    private String decodeRTypeInstruction(int instructionOf32){
+
+        int opcode = (instructionOf32 >> 21) & 0x7ff;
+        int Rm = (instructionOf32 >> 16) & 0x1f;
+        int shamt = (instructionOf32 >> 10) & 0x3f;
+        int Rn = (instructionOf32 >> 5) & 0x1f;
+        int Rd = instructionOf32 & 0x1f;
+
+        String RdRnRm = binaryToRegister(Rd) + ", " + binaryToRegister(Rn) + ", " + binaryToRegister(Rm);
+
+        String instruction;
+        String completeInstruction;
+
+        switch (opcode){
+
+            case 0b10001011000:
+                instruction = "ADD";
+                completeInstruction = (instruction + " " + RdRnRm);
+
+            case 0b10001010000:
+                instruction = "AND";
+                completeInstruction = (instruction + " " + RdRnRm);
+
+            case 0b11010110000:
+                instruction = "BR";
+                completeInstruction = (instruction + " X" + binaryToDecimal())
+            case 0b11001010000:
+                instruction = "EOR";
+            case 0b11010011011:
+                instruction = "LSL";
+            case 0b11010011010:
+                instruction = "LSR";
+            case 0b10101010000:
+                instruction = "ORR";
+            case 0b11001011000:
+                instruction = "SUB";
+            case 0b11101011000:
+                instruction = "SUBS";
+            case 0b10011011000:
+                instruction = "MUL";
+            case 0b11111111101:
+                instruction = "PRNT";
+            case 0b11111111100:
+                instruction = "PRNL";
+            case 0b11111111110:
+                instruction = "DUMP";
+            case 0b11111111111:
+                instruction = "HALT";
+            default:
+                completeInstruction = "INVALID";
+        }
+        return completeInstruction;
+    }
+
+    private String decodeITypeInstruction(){}
+
+    private String decodeDTypeInstruction(){}
+
+    private String decodeBTypeInstruction(){}
+
+    private String decodeCBTypeInstruction(){}
+
+    /**
+     * Converts binary to register
+     * @param binary
+     * @return
+     */
+    private String binaryToRegister (int binary){
+
+        int decimal = 0;
+        int position = 0;
+
+        while (binary > 0){
+
+            int bit = binary & 0x1;
+            decimal += bit * Math.pow(2, position);
+
+            binary = binary >> 1;
+            position++;
+        }
+
+        return switch (decimal) {
+            case 28 -> "SP";
+            case 29 -> "FP";
+            case 30 -> "LR";
+            case 31 -> "XZR";
+            default -> "X" + decimal;
+        };
+    }
+
 
 
 
